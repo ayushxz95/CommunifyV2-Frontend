f0e9038b586c63938259d6b777082f868d2d7a5e
ayushxz95 <ayushxyz95@gmail.com>
2024-03-05 23:32:18 +0530

    Initial changes

---
+++ communify-frontend/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center text-3xl font-bold text-indigo-500 ">
+      Welcome to communify
+    </div>
+  );
+}
